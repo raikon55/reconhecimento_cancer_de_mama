e160719a8f0a1d369f49da8918b656beb3ad1c88
raikon55 <edufelopes@gmail.com>
2021-04-25 12:41:13 -0300

    Implementacao do zoom selecionado por uma regiao retangular

--- src/handler/ImageHandler.py
+++ src/handler/ImageHandler.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from PIL import Image
+from PIL import Image, ImageQt
 
 
 class ImageHandler:
@@ -8,10 +8,10 @@ class ImageHandler:
     ZOOM_RATIO: tuple = (0, 0, 128, 128)
 
     __ratioOriginalToInterface: tuple
-    __interfaceImage: Image
-    __originalImage: Image
     __tempImage: Image = None
     __in_zoom: bool = False
+    __interfaceImage: Image
+    __originalImage: Image
     __filename: str
 
     def __init__(self, filename: str):
@@ -23,10 +23,13 @@ class ImageHandler:
     def get_image(self, original: bool = False) -> Image:
         return self.__originalImage if original else self.__interfaceImage
 
-    def get_file_path(self):
+    def get_qt_image(self, original: bool = False) -> ImageQt:
+        return ImageQt.toqimage(self.normalize(self.__originalImage)) if original else ImageQt.toqimage(self.__interfaceImage)
+
+    def get_file_path(self) -> str:
         return self.__filename
 
-    def normalize(self, image: Image):
+    def normalize(self, image: Image) -> Image:
         ratio = (
             image.width // self.IMAGE_SIZE[0],
             image.height // self.IMAGE_SIZE[1]
@@ -40,16 +43,18 @@ class ImageHandler:
             self.IMAGE_SIZE if ratio[0] > 1 and ratio[1] > 1 else image.size
         )
 
+        return self.__interfaceImage
+
     def zoom_in(self, region: tuple = (0, 0)) -> Image:
         self.temporary_image(True)
+        print(region)
         zoom_region = (
-            self.ZOOM_RATIO[0] + region[0],
-            self.ZOOM_RATIO[1] + region[1],
-            self.ZOOM_RATIO[2] + region[0],
-            self.ZOOM_RATIO[3] + region[1]
+            self.ZOOM_RATIO[0] + region[0], self.ZOOM_RATIO[1] + region[1],
+            (self.ZOOM_RATIO[2] + region[0]) * , self.ZOOM_RATIO[3] + region[1]
         )
 
         self.__interfaceImage = self.__interfaceImage.crop(zoom_region).resize(self.IMAGE_SIZE)
+
         return self.__interfaceImage
 
     def zoom_out(self) -> Image:
@@ -62,19 +67,21 @@ class ImageHandler:
             self.__interfaceImage = self.__interfaceImage.resize((new_size, new_size))
         return self.__interfaceImage
 
-    def temporary_image(self, is_zoom: bool):
+
+
+    def temporary_image(self, is_zoom: bool) -> None:
         if self.__tempImage is None and is_zoom:
             self.__tempImage = self.__interfaceImage.copy()
             self.__in_zoom = True
         else:
-            if self.__tempImage is not None:
+            if self.__tempImage is not None and is_zoom:
                 self.__interfaceImage = self.__tempImage.copy()
             else:
                 self.normalize(self.__originalImage)
             self.__tempImage = None
             self.__in_zoom = False
 
-    def get_info(self):
+    def get_info(self) -> None:
         print('\nAttributes viewed image\n'
               f'Image resolution:     {self.__interfaceImage.size}\n'
               f'Columns:              {self.__interfaceImage.width}\n'
@@ -94,7 +101,7 @@ class ImageHandler:
               f'Height ratio: {self.__ratioOriginalToInterface[1]}\n'
               )
 
-    def close(self):
+    def close(self) -> None:
         self.__interfaceImage.close()
         self.__originalImage.close()
         if self.__tempImage is not None:
--- src/view/ImageInterface.py
+++ src/view/ImageInterface.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from PIL.ImageQt import ImageQt
 from PySide6 import QtCore
-from PySide6.QtCore import QRect, QMetaObject, QCoreApplication
-from PySide6.QtGui import QPixmap, QAction
+from PySide6.QtCore import QRect, QMetaObject, QCoreApplication, QPoint
+from PySide6.QtGui import QPixmap, QAction, QPainter, QPen, QImage, QMouseEvent, QPaintEvent
 from PySide6.QtWidgets import QLabel, QPushButton, QWidget, QMenuBar, QButtonGroup, QFrame, QMenu, QStatusBar, \
-    QMainWindow, QMdiArea, QLineEdit, QInputDialog
+    QMainWindow, QInputDialog, QRubberBand, QDialog, QMessageBox
 
 from ..handler.ImageHandler import ImageHandler
 
@@ -20,11 +20,16 @@ class ImageInterface(QMainWindow):
     __resize32x32Button: QPushButton
     __resize64x64Button: QPushButton
     __equalizeButton: QPushButton
+    __zoomOutButton: QPushButton
+    __zoomInButton: QPushButton
 
     # Actions
     __actionOpenImage: QAction
     __actionCloseImage: QAction
     __actionQuit: QAction
+
+    __point: QPoint
+    __rubberBand: QRubberBand
 
     # Menu
     __menuBar: QMenuBar
@@ -44,6 +49,7 @@ class ImageInterface(QMainWindow):
         super().__init__()
         self.setObjectName(u'ImagePy')
         self.resize(1280, 720)
+        self.setMouseTracking(True)
 
         self.__imageWidget = QWidget(self)
 
@@ -52,6 +58,8 @@ class ImageInterface(QMainWindow):
         self.__resize32x32Button = QPushButton(self.__imageWidget)
         self.__resize64x64Button = QPushButton(self.__imageWidget)
         self.__equalizeButton = QPushButton(self.__imageWidget)
+        self.__zoomOutButton = QPushButton(self.__imageWidget)
+        self.__zoomInButton = QPushButton(self.__imageWidget)
 
         self.__actionOpenImage = QAction(self)
         self.__actionCloseImage = QAction(self)
@@ -62,17 +70,22 @@ class ImageInterface(QMainWindow):
 
         self.__statusBar = QStatusBar(self)
 
-        self.__imageLabel = QLabel(self.__imageWidget, alignment=QtCore.Qt.AlignCenter)
+        self.__imageLabel = ZoomLabel(self.__imageWidget, alignment=QtCore.Qt.AlignCenter)
 
         # Init interface
         self.setup_ui()
 
     def open_image(self) -> None:
-        file_path, ok = QInputDialog.getText(self, u'text', u'Enter image path')
+        try:
+            file_path, ok = QInputDialog.getText(self, u'text', u'Enter image path')
 
-        self.__filename = file_path if ok else None
-        self.__imageHandler = ImageHandler(self.__filename)
-        self.load_image(can_update=True)
+            self.__filename = file_path if ok else None
+            self.__imageHandler = ImageHandler(self.__filename)
+            self.load_image(can_update=True)
+        except FileNotFoundError:
+            msgBox = QMessageBox(self)
+            msgBox.setText(u'Image path not found')
+            msgBox.exec_()
 
     @QtCore.Slot()
     def load_image(self, can_update: bool = False) -> None:
@@ -80,18 +93,24 @@ class ImageInterface(QMainWindow):
             return
 
         elif self.__containerImagePixmap is None or can_update:
-            self.__containerImagePixmap = QPixmap(self.__imageHandler.get_file_path())
+            q_image: QImage = self.__imageHandler.get_qt_image(original=False)
+            self.__containerImagePixmap = QPixmap.fromImage(q_image)
             self.__imageLabel.setPixmap(self.__containerImagePixmap)
 
         elif self.__containerImagePixmap is not None \
                 and self.__imageModified is not None \
                 and self.__imageModified != self.__imageHandler.get_image():
-            self.__containerImagePixmap = QPixmap(self.__imageModified)
+            q_image: QImage = self.__imageHandler.get_qt_image(original=False)
+            self.__containerImagePixmap = QPixmap.fromImage(q_image)
             self.__imageLabel.setPixmap(self.__containerImagePixmap)
 
     def recover_original_image(self) -> None:
-        self.__containerImagePixmap = QPixmap(self.__imageHandler.get_file_path())
-        self.__imageLabel.setPixmap(self.__containerImagePixmap)
+        try:
+            q_image: QImage = self.__imageHandler.get_qt_image(original=True)
+            self.__containerImagePixmap = QPixmap.fromImage(q_image)
+            self.__imageLabel.setPixmap(self.__containerImagePixmap)
+        except AttributeError:
+            return
 
     def close_image(self) -> None:
         self.__imageHandler = None
@@ -101,24 +120,55 @@ class ImageInterface(QMainWindow):
     def create_interface(self) -> None:
         super().show()
 
-    # def zoom_out(self) -> None:
-    #     print('Zoom out')
+    def zoom_out(self) -> None:
+        try:
+            zoom_image: ImageHandler = self.__imageHandler.zoom_out()
+            self.__imageModified = ImageQt(zoom_image)
+            self.load_image()
+        except AttributeError:
+            return
 
-    # def zoom_in(self) -> None:
-    #     print('Zoom in')
+    def zoom_in(self) -> None:
+        try:
+            zoom_image: ImageHandler = self.__imageHandler.zoom_in(self.__imageLabel.get_initial_point())
+            self.__imageModified = ImageQt(zoom_image)
+            self.load_image()
+        except AttributeError:
+            return
 
     def decrease_to_64(self) -> None:
-        self.__imageModified = ImageQt(self.__imageHandler.new_resolution(64))
-        self.load_image()
+        try:
+            self.__imageModified = ImageQt(self.__imageHandler.new_resolution(64))
+            self.load_image()
+        except AttributeError:
+            return
 
     def decrease_to_32(self) -> None:
-        self.__imageModified = ImageQt(self.__imageHandler.new_resolution(32))
-        self.load_image()
+        try:
+            self.__imageModified = ImageQt(self.__imageHandler.new_resolution(32))
+            self.load_image()
+        except AttributeError:
+            return
+
+    def equalize(self) -> None:
+        print(u'equalize')
 
     def is_equal_image(self, filename: str) -> bool:
         return self.__filename == filename
 
     def setup_ui(self) -> None:
+        button_place: int = 0
+        distance_between_buttons: int = 20
+        column_default_position_button: int = 80
+
+        image_label_position: int = column_default_position_button - 50
+        image_label_width: int = int(self.width() * 0.8)
+        image_label_height: int = int(self.height() * 0.8)
+
+        width_button: int = 80
+        height_button: int = 25
+        row_position_button: int = image_label_height + 30
+
         self.__actionOpenImage.setObjectName(u'__actionOpenImage')
         self.__actionOpenImage.triggered.connect(self.open_image)
 
@@ -132,27 +182,50 @@ class ImageInterface(QMainWindow):
 
         self.__imageManipulationGroup.setObjectName(u'__imageManipulationGroup')
 
-        self.__resize64x64Button.setObjectName(u'__resize64x64Button')
-        self.__resize64x64Button.setGeometry(QRect(311, 470, 78, 25))
-        self.__resize64x64Button.clicked.connect(self.decrease_to_64)
-        self.__imageManipulationGroup.addButton(self.__resize64x64Button)
-
         self.__resize32x32Button.setObjectName(u'__resize32x32Button')
-        self.__resize32x32Button.setGeometry(QRect(230, 470, 77, 25))
+        self.__resize32x32Button.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button,
+                  height_button))
         self.__resize32x32Button.clicked.connect(self.decrease_to_32)
         self.__imageManipulationGroup.addButton(self.__resize32x32Button)
 
+        button_place += (width_button + distance_between_buttons)
+        self.__resize64x64Button.setObjectName(u'__resize64x64Button')
+        self.__resize64x64Button.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button, height_button))
+        self.__resize64x64Button.clicked.connect(self.decrease_to_64)
+        self.__imageManipulationGroup.addButton(self.__resize64x64Button)
+
+        button_place += (width_button + distance_between_buttons)
         self.__equalizeButton.setObjectName(u'__equalizeButton')
-        self.__equalizeButton.setGeometry(QRect(474, 470, 77, 25))
+        self.__equalizeButton.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button, height_button))
+        self.__equalizeButton.clicked.connect(self.equalize)
         self.__imageManipulationGroup.addButton(self.__equalizeButton)
 
+        button_place += (width_button + distance_between_buttons)
         self.__originalImageButton.setObjectName(u'__originalImageButton')
-        self.__originalImageButton.setGeometry(QRect(393, 470, 77, 25))
+        self.__originalImageButton.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button, height_button))
         self.__originalImageButton.clicked.connect(self.recover_original_image)
         self.__imageManipulationGroup.addButton(self.__originalImageButton)
 
+        button_place += (width_button + distance_between_buttons)
+        self.__zoomInButton.setObjectName(u'__zoomInButton')
+        self.__zoomInButton.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button, height_button))
+        self.__zoomInButton.clicked.connect(self.zoom_in)
+        self.__imageManipulationGroup.addButton(self.__zoomInButton)
+
+        button_place += (width_button + distance_between_buttons)
+        self.__zoomOutButton.setObjectName(u'__zoomOutButton')
+        self.__zoomOutButton.setGeometry(
+            QRect(column_default_position_button + button_place, row_position_button, width_button, height_button))
+        self.__zoomOutButton.clicked.connect(self.zoom_out)
+        self.__imageManipulationGroup.addButton(self.__zoomOutButton)
+
         self.__imageLabel.setObjectName(u'__imageLabel')
-        self.__imageLabel.setGeometry(QRect(20, 20, 671, 431))
+        self.__imageLabel.setGeometry(QRect(image_label_position, 20, image_label_width, image_label_height))
         self.__imageLabel.setFrameShape(QFrame.StyledPanel)
         self.__imageLabel.setFrameShadow(QFrame.Raised)
 
@@ -172,12 +245,12 @@ class ImageInterface(QMainWindow):
         self.__menuFile.addAction(self.__actionCloseImage)
         self.__menuFile.addAction(self.__actionQuit)
 
-        self.retranslate_ui()
+        self.translate_ui()
 
         QMetaObject.connectSlotsByName(self)
 
-    def retranslate_ui(self) -> None:
-        self.setWindowTitle(QCoreApplication.translate('MainWindow', u'MainWindow', None))
+    def translate_ui(self) -> None:
+        self.setWindowTitle(QCoreApplication.translate('MainWindow', u'ImagePy', None))
 
         self.__menuFile.setTitle(QCoreApplication.translate('MainWindow', u'File', None))
         self.__actionOpenImage.setText(QCoreApplication.translate('MainWindow', u'Open Image...', None))
@@ -188,3 +261,39 @@ class ImageInterface(QMainWindow):
         self.__resize32x32Button.setText(QCoreApplication.translate('MainWindow', u'32x32', None))
         self.__originalImageButton.setText(QCoreApplication.translate('MainWindow', u'Original', None))
         self.__equalizeButton.setText(QCoreApplication.translate('MainWindow', u'Equalize', None))
+        self.__zoomInButton.setText(QCoreApplication.translate('MainWindow', u'Zoom In', None))
+        self.__zoomOutButton.setText(QCoreApplication.translate('MainWindow', u'Zoom Out', None))
+
+
+class ZoomLabel(QLabel):
+    __x: int = 0
+    __y: int = 0
+    __flag: bool = False
+
+    def __init__(self, widget: QWidget, alignment: QtCore.Qt.AlignmentFlag):
+        super().__init__(widget, alignment=alignment)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        self.__flag = True
+        self.__x = event.x()
+        self.__y = event.y()
+        self.update()
+
+    def mouseReleaseEvent(self, event: QPaintEvent) -> None:
+        self.__flag = False
+
+    def mouseMoveEvent(self, event: QPaintEvent) -> None:
+        if self.__flag:
+            self.__x = event.x()
+            self.__y = event.y()
+            self.update()
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        super().paintEvent(event)
+        rect = QRect(self.__x, self.__y, 128, 128)
+        painter: QPainter = QPainter(self)
+        painter.setPen(QPen(QtCore.Qt.blue, 1, QtCore.Qt.SolidLine))
+        painter.drawRect(rect)
+
+    def get_initial_point(self) -> tuple:
+        return self.__x, self.__y
